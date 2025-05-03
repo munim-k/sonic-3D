d82ba055b3e154c8c9401e7fc0390482ed7a1320
Muhammad Bilal <muhammad.bilal832017@gmail.com>
2025-05-03 16:38:13 +0500

    added aiming feature

---
+++ Assets/Ragdoll Engine/Profiles/PumpkinDog/Aiming/AimingVisual.cs
@@ -0,0 +1,37 @@
+using RagdollEngine;
+using UnityEngine;
+
+public class AimingVisual : MonoBehaviour
+{
+    [SerializeField] private GameObject aimTarget;
+    [SerializeField] private PumpkinDogAimPlayerBehaviour aimBehaviour;
+    [SerializeField] private LineRenderer lineRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        if (aimBehaviour != null)
+        {
+            // Get the points from the aim behaviour
+            var points = aimBehaviour.getPoints();
+            // Set the position of the aim target to the last point
+            if (points != null)
+            {
+                if (points.Count > 0)
+                {
+                    aimTarget.transform.position = points[points.Count - 1];
+                    lineRenderer.positionCount = points.Count;
+                    for (int i = 0; i < points.Count; i++)
+                    {
+                        lineRenderer.SetPosition(i, points[i]);
+                    }
+                }
+
+            }
+        }
+    }
+}
--- Assets/Ragdoll Engine/Scripts/Player Behaviour Tree/Player Behaviours/PumpkinDog Player Behaviours/PumpkinDogAimPlayerBehaviour.cs
+++ Assets/Ragdoll Engine/Scripts/Player Behaviour Tree/Player Behaviours/PumpkinDog Player Behaviours/PumpkinDogAimPlayerBehaviour.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,7 +8,10 @@ namespace RagdollEngine
     public class PumpkinDogAimPlayerBehaviour : PlayerBehaviour
     {
         [SerializeField] private float throwForce; // Force applied to the projectile
-
+        [SerializeField] private int maxPoints; // Maximum number of points for projectile motion
+        [SerializeField] private float scale;
+        [SerializeField] private Vector3 offsetVector;
+        private List<Vector3> points;// List to store points for projectile motion
         bool aiming;
         public override bool Evaluate()
         {
@@ -23,9 +28,67 @@ namespace RagdollEngine
 
         public override void Execute()
         {
-           //Do raycasts for projectile motion based on the camera angle
+            if(points == null)
+            {
+                points = new List<Vector3>();
+            }
+            points.Clear();
+            // Calculate the vertical angle between the camera and the player
+            Vector3 cameraForward = cameraTransform.forward;
+            Vector3 playerForward = modelTransform.forward;
+            float verticalAngle = Vector3.SignedAngle(Vector3.ProjectOnPlane(cameraForward, modelTransform.right), playerForward, modelTransform.right);
+            //Invert the angle to make it positive
+            verticalAngle = -verticalAngle;
+            // Adjust the throw direction based on the vertical angle
+            Vector3 adjustedThrowDirection = Quaternion.AngleAxis(verticalAngle, modelTransform.right) * modelTransform.forward;
+
+            Vector3 newPosition = modelTransform.position + (modelTransform.rotation * offsetVector);
+
+            //Do raycasts for projectile motion in the forward direction
+            for (int i = 0; i < maxPoints; i++) {
+                //Calculate the time of flight for each point
+                float t = i * Time.fixedDeltaTime*scale;
+                //Calculate the position of the projectile at time t
+                Vector3 point = newPosition +
+                      (adjustedThrowDirection * throwForce * t) +
+                      (0.5f * Physics.gravity * t * t);
+                points.Add(point);
+            }
+            //Do a raycast forward from each point to the next point
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(points[i], points[i + 1] - points[i], out hit, Vector3.Distance(points[i], points[i + 1])))
+                {
+                   
+                    //Removeall points after the hit point
+                    points.RemoveRange(i + 1, points.Count - (i + 1));
+                    points.Add(hit.point);
+                    break;
+                }
+            }
+            
 
         }
+
+
+        public List<Vector3> getPoints()
+        {
+            return points;
+        }
+
+        public Vector3 getHitPoint()
+        {
+            if (points.Count > 0)
+            {
+                return points[points.Count - 1];
+            }
+            else
+            {
+                return Vector3.zero;
+            }
+        }
+
 
 
         
