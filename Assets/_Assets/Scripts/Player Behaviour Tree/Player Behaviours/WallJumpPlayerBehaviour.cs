7bf29f237e1e800aa1a1d2297dee8a628565e30c
Muhammad Bilal <muhammad.bilal832017@gmail.com>
2025-05-09 20:23:25 +0500

    Fixed walljump

--- Assets/_Assets/Scripts/Player Behaviour Tree/Player Behaviours/WallJumpPlayerBehaviour.cs
+++ Assets/_Assets/Scripts/Player Behaviour Tree/Player Behaviours/WallJumpPlayerBehaviour.cs
@@ -5,33 +5,201 @@ namespace RagdollEngine
     public class WallJumpPlayerBehaviour : PlayerBehaviour
     {
         [SerializeField] float wallJumpCooldown; // Cooldown time between wall jumps.
-        [SerializeField] float wallJumpForce; // Force applied during a wall jump.
-        [SerializeField] float wallJumpSpeed;
+        [SerializeField] float wallJumpForceHorizontal; // Force applied during a wall jump.
+        [SerializeField] float wallJumpForceVertical; // Force applied during a wall jump.
+        [SerializeField] float wallJumpTime;
+        [SerializeField] float wallJumpDecreaseSpeed;
         [SerializeField] LayerMask wallLayerMask; // Layer mask to identify walls.
 
-        float wallJumpCooldownTimer = 0;
-        bool wallJumping; // Indicates if the player is currently wall jumping.
-        Vector3 wallNormal; // Stores the normal of the wall the player is in contact with.
+        float speed = 0;
         Vector3 goalPosition; // The target position the player moves toward during the wall jump.
-        Vector3 origPos;
-        float walljumpLerp = 0f;
+        Vector3 jumpVector;
+        Vector3 wallPoint;
+        Vector3 wallNormal;
+        RaycastHit hit;
+
+        // Tracks the current length of the spring
+        float currentLength;
+
+        // Speed of the springing motion
+
+
+        //public override bool Evaluate()
+        //{
+        //    if (wallJumpCooldownTimer > 0)
+        //    {
+        //        wallJumpCooldownTimer -= Time.deltaTime;
+        //    }
+        //    // Check if the player is in contact with a wall.
+        //    //Do raycasts in all 4 directions and use the first one to hit
+        //    if (!wallJumping)
+        //    {
+        //        bool wallContact = false;
+        //        wallContact = Physics.Raycast(
+        //           playerTransform.position, // Start position of the raycast.
+        //           playerTransform.forward, // Direction of the raycast (forward).
+        //           out RaycastHit hit, // Stores information about the object hit by the raycast.
+        //           height + Physics.defaultContactOffset, // Raycast distance.
+        //           wallLayerMask, // Layer mask to filter walls.
+        //           QueryTriggerInteraction.Ignore // Ignore trigger colliders.
+        //       );
+        //        if (!wallContact)
+        //        {
+        //            wallContact = Physics.Raycast(
+        //                playerTransform.position, // Start position of the raycast.
+        //                -playerTransform.forward, // Direction of the raycast (backward).
+        //                out hit, // Stores information about the object hit by the raycast.
+        //                height + Physics.defaultContactOffset, // Raycast distance.
+        //                wallLayerMask, // Layer mask to filter walls.
+        //                QueryTriggerInteraction.Ignore // Ignore trigger colliders.
+        //            );
+        //        }
+        //        if (!wallContact)
+        //        {
+        //            wallContact = Physics.Raycast(
+        //                playerTransform.position, // Start position of the raycast.
+        //                playerTransform.right, // Direction of the raycast (right).
+        //                out hit, // Stores information about the object hit by the raycast.
+        //                height + Physics.defaultContactOffset, // Raycast distance.
+        //                wallLayerMask, // Layer mask to filter walls.
+        //                QueryTriggerInteraction.Ignore // Ignore trigger colliders.
+        //            );
+        //        }
+        //        if (!wallContact)
+        //        {
+        //            wallContact = Physics.Raycast(
+        //                playerTransform.position, // Start position of the raycast.
+        //                -playerTransform.right, // Direction of the raycast (left).
+        //                out hit, // Stores information about the object hit by the raycast.
+        //                height + Physics.defaultContactOffset, // Raycast distance.
+        //                wallLayerMask, // Layer mask to filter walls.
+        //                QueryTriggerInteraction.Ignore // Ignore trigger colliders.
+        //            );
+        //        }
+        //        if (wallContact)
+        //        {
+        //            // Store the wall normal and reset the wall jump timer.
+        //            wallNormal = hit.normal;
+        //            if (inputHandler.jump.pressed && wallJumpCooldownTimer <= 0)
+        //            {
+        //                StartWallJump();
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        PerformWallJump();
+        //    }
+        //    //If the player is on the ground
+        //    if (groundInformation.ground)
+        //    {
+        //        wallJumping = false; // Exit wall jump state.
+        //        kinematic = false; // Exit kinematic mode.
+        //        overrideModelTransform = false; // Allow other behaviors to control the model transform.
+        //    }
+        //    return wallJumping;
+        //}
+        //private void StartWallJump()
+        //{
+        //    // Calculate the goal position for the wall jump.
+        //    origPos = playerTransform.position;
+        //    goalPosition = playerTransform.position + (wallNormal + Vector3.up).normalized * wallJumpForce;
+        //    jumpVelocity = wallNormal * wallJumpForce * jumpVelocityMul;
+
+        //    //Raycast towards the goal position and shorten it if necessary
+        //    if (Physics.Raycast(playerTransform.position, goalPosition - playerTransform.position, out RaycastHit hit, (goalPosition - playerTransform.position).magnitude, wallLayerMask, QueryTriggerInteraction.Ignore))
+        //    {
+        //        goalPosition = hit.point;
+        //    }
+
+        //    // Trigger jump animation.
+        //    animator.SetTrigger("Jump");
+
+        //    // Set the player to kinematic mode.
+        //    walljumpLerp = 0f;
+        //    kinematic = true;
+        //    wallJumping = true;
+        //    overrideModelTransform = true; // Override the model transform to control the player's position.
+        //    wallJumpCooldownTimer = wallJumpCooldown;
+        //}
+
+        //private void PerformWallJump()
+        //{
+        //    // Move the player toward the goal position.
+        //    Vector3 difference = goalPosition - playerTransform.position;
+
+
+        //    // If the player has reached the goal position, end the wall jump.
+        //    if (difference.magnitude <= 0.1f)
+        //    {
+        //        walljumpLerp = 0f; // Reset the lerp value.
+        //        wallJumping = false;
+        //        kinematic = false; // Exit kinematic mode.
+        //        overrideModelTransform = false; // Allow other behaviors to control the model transform.
+        //        additiveVelocity += jumpVelocity;
+        //        return;
+        //    }
+
+
+        //    // Update the player's position to move toward the goal
+        //    walljumpLerp += Time.fixedDeltaTime * wallJumpSpeed;
+        //    playerTransform.position = Vector3.Lerp(origPos, goalPosition, walljumpLerp); // Smooth movement.
+        //    playerTransform.rotation = Quaternion.LookRotation(difference); // Rotate the model to face the goal position.
+        //    Debug.DrawLine(origPos, goalPosition, Color.red); // Draw a line in the editor for visualization.
+        //}
+
         public override bool Evaluate()
         {
-            if (wallJumpCooldownTimer > 0)
+            // Check if the player can interact with a spring
+            if (!WallCheck()) return false;
+
+            // Calculate the goal position for the player based on the spring's position and length
+            Vector3 goal = wallPoint +
+                (jumpVector * (wallJumpTime - currentLength));
+
+            // Set the player's target position
+            movePosition = goal;
+
+            // Make the player kinematic (disable physics-based movement)
+            kinematic = true;
+
+            // Align the player's model to face the wall's direction
+            modelTransform.rotation = Quaternion.LookRotation(jumpVector,modelTransform.up);
+
+            // Position the player's model at the calculated goal position
+            modelTransform.position = goal - (modelTransform.up * height);
+
+            // Override the model's transform to prevent other behaviors from modifying it
+            overrideModelTransform = true;
+
+            // Reduce the current length of the spring based on speed and velocity
+            currentLength = Mathf.Max(currentLength - speed,0);
+
+            // If the spring has fully compressed, deactivate the behavior
+            if (currentLength <= 0)
+                return false;
+            return true; // The behavior remains active
+        }
+
+        // Checks if the player can interact with a Wall
+        bool WallCheck()
+        {
+            // Iterate through all stage objects in the scene
+
+            // Check if the stage object is a spring
+            if (wallJumpCooldown >= 0)
             {
-                wallJumpCooldownTimer -= Time.deltaTime;
+                wallJumpCooldown -= Time.fixedDeltaTime;
+
             }
-            // Check if the player is in contact with a wall.
-            //Do raycasts in all 4 directions and use the first one to hit
-            if (!wallJumping)
+            else
             {
+
                 bool wallContact = false;
-
-
                 wallContact = Physics.Raycast(
                    playerTransform.position, // Start position of the raycast.
                    playerTransform.forward, // Direction of the raycast (forward).
-                   out RaycastHit hit, // Stores information about the object hit by the raycast.
+                   out hit, // Stores information about the object hit by the raycast.
                    height + Physics.defaultContactOffset, // Raycast distance.
                    wallLayerMask, // Layer mask to filter walls.
                    QueryTriggerInteraction.Ignore // Ignore trigger colliders.
@@ -69,78 +237,26 @@ namespace RagdollEngine
                         QueryTriggerInteraction.Ignore // Ignore trigger colliders.
                     );
                 }
-                if (wallContact)
+                if (wallContact && inputHandler.jump.pressed)
                 {
-                    // Store the wall normal and reset the wall jump timer.
+                    // Set the current length of the spring
+                    currentLength = wallJumpTime;
+                    speed = wallJumpDecreaseSpeed;
+                    // Calculate the additive velocity to apply to the player
+                    wallPoint = hit.point;
                     wallNormal = hit.normal;
-                    if (inputHandler.jump.pressed && wallJumpCooldownTimer <= 0)
-                    {
-                        StartWallJump();
-                    }
+                    jumpVector = wallNormal.normalized * wallJumpForceHorizontal;
+                    jumpVector += Vector3.up * wallJumpForceVertical;
+                    additiveVelocity = -RB.linearVelocity + jumpVector;
+                    goalPosition = playerTransform.position + jumpVector;
+                    return true; // The player can interact with the spring
                 }
+
             }
-            else
-            {
-                PerformWallJump();
-            }
-            //If the player is on the ground
-            if (groundInformation.ground)
-            {
-                wallJumping = false; // Exit wall jump state.
-                kinematic = false; // Exit kinematic mode.
-                overrideModelTransform = false; // Allow other behaviors to control the model transform.
-            }
-            return wallJumping;
-        }
-        private void StartWallJump()
-        {
-            // Calculate the goal position for the wall jump.
-            origPos=playerTransform.position;
-            goalPosition = playerTransform.position + (wallNormal + Vector3.up).normalized * wallJumpForce;
-            //Raycast towards the goal position and shorten it if necessary
-           if(Physics.Raycast(playerTransform.position, goalPosition - playerTransform.position, out RaycastHit hit, (goalPosition - playerTransform.position).magnitude, wallLayerMask, QueryTriggerInteraction.Ignore))
-            {
-                goalPosition = hit.point;
-            }
+            // If the player was previously active and the spring is still compressing, return true
+            if (wasActive && currentLength > 0) return true;
 
-            // Trigger jump animation.
-            animator.SetTrigger("Jump");
-
-            // Set the player to kinematic mode.
-            walljumpLerp = 0f;
-            kinematic = true;
-            wallJumping = true;
-            overrideModelTransform = true; // Override the model transform to control the player's position.
-            wallJumpCooldownTimer = wallJumpCooldown;
-        }
-
-        private void PerformWallJump()
-        {
-            // Move the player toward the goal position.
-            Vector3 difference = goalPosition - playerTransform.position;
-
-
-            // If the player has reached the goal position, end the wall jump.
-            if (difference.magnitude <= 0.1f)
-            {
-                walljumpLerp = 0f; // Reset the lerp value.
-                wallJumping = false;
-                kinematic = false; // Exit kinematic mode.
-                overrideModelTransform = false; // Allow other behaviors to control the model transform.
-                return;
-            }
-
-
-            // Update the player's position to move toward the goal
-            walljumpLerp += Time.fixedDeltaTime * wallJumpSpeed;
-            playerTransform.position = Vector3.Lerp(origPos, goalPosition, walljumpLerp); // Smooth movement.
-            playerTransform.rotation = Quaternion.LookRotation(difference); // Rotate the model to face the goal position.
-            Debug.DrawLine(origPos, goalPosition, Color.red); // Draw a line in the editor for visualization.
-        }
-
-        private void OnDrawGizmos()
-        {
-            Gizmos.DrawSphere(goalPosition, 0.5f); // Draw a sphere at the goal position for visualization.
+            return false; // The player cannot interact with a spring
         }
     }
 }
