83664891808e5668bf72891c49e81727d3d8a350
Muhammad Bilal <muhammad.bilal832017@gmail.com>
2025-08-21 07:06:04 +0500

    added helicopter bot visual

---
+++ Assets/_Assets/Scripts/Enemies/HelicopterBotEnemy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelicopterBotEnemy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
